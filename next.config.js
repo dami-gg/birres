8b7b8be39ed05593e29e48e035f0b78dcdb7b695
Dami <damian.garciagarcia@gmail.com>
2019-09-19 14:42:41 +0200

    Fix deployment

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ const { parsed: localEnv } = require("dotenv").config();
 const webpack = require("webpack"); // eslint-disable-line
 
 module.exports = {
-  distDir: "public/_next",
-  target: "serverless",
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
 
--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const FIREBASE_CONFIG = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: "AIzaSyAxomPcdjUYjos_6-Vcjj07sazCVpoQBHE",
   authDomain: "beer-collection.firebaseapp.com",
   databaseURL: "https://beer-collection.firebaseio.com",
   projectId: "beer-collection",
   storageBucket: "beer-collection.appspot.com",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  messagingSenderId: "549305960758"
 };
 
 if (!firebase.apps.length) {
