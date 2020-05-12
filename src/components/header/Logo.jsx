5dcc1edf93594c4ec58d221b2920648f5aad4aa3
Dami <damian.garciagarcia@gmail.com>
2020-05-12 11:36:35 +0200

    Make logo navigate to home page

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Link from "next/link";
 import { makeStyles } from "@material-ui/core/styles";
 
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   logo: {
-    display: "flex"
+    display: "flex",
+    cursor: "pointer"
   },
   imageWrapper: {
     marginRight: 8
@@ -19,14 +21,16 @@ const Logo = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.logo}>
-      <div className={classes.imageWrapper}>
-        <img src="/images/logo.png" height="32" width="32" alt="logo" />
+    <Link href="/">
+      <div className={classes.logo}>
+        <div className={classes.imageWrapper}>
+          <img src="/images/logo.png" height="32" width="32" alt="logo" />
+        </div>
+        <Typography variant="h6" className={classes.text}>
+          Birres
+        </Typography>
       </div>
-      <Typography variant="h6" className={classes.text}>
-        Birres
-      </Typography>
-    </div>
+    </Link>
   );
 };
 
